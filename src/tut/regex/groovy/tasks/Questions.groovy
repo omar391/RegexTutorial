6541c84f0fe3bd26c2bb4fcae43cc96d37cde466
omar <omar@omar-NV55C>
2014-07-08 12:28:25 +0600

    Revert "Revert "some fixing""
    This reverts commit 4cc0405b54642c327405e785fd0fe0fbe1d5b446.

--- src/tut/regex/groovy/tasks/Questions.groovy
+++ src/tut/regex/groovy/tasks/Questions.groovy
@@ -1,8 +1,61 @@
 package tut.regex.groovy.tasks
 
 class Questions {
-	
-/**
+
+	/**
+	 * All the daily task (questions) will be assigned here.
+	 */
+
+/*
+
+1. Count duplicate words in a sentence|file|string block input.
+in : "he he goes to school"
+out: he: 2, goes: 1, school: 1
+ 
+2. Check whether an email address is valid or not.
+in : x@x.com out: valid
+in : x@x@.com out: invalid
+in : x@x.com.bd.gov out: valid
+in : x.x@x.com out: valid
+in : ~x@x.com out: invalid
+in : "x@"x.com out: invalid
+
+3. Make a Ip(v4) address validator method.
+in : 10.10.10.10 out: valid 
+in : 310.10.10.10 out: invalid 
+in : 255.10.10.10 out: valid 
+in : 2555.10.10.10 out: invalid 
+
+4. Check if given time format is valid or not. Format: hh:mm:ss [12 hrs]
+in : 12.59.59 out: valid
+in : 12.60.59 out: invalid
+
+5. Check if given date format is valid or not. Format: dd/mm/yyyy [yyyy is valid upto 2014]
+in : 12/12/1212 : valid
+in : 12/13/1212 : invalid
+in : 12/12/12122 : invalid
+in : 32/12/1212 : invalid
+
+6. Check whether a html block is unique in a html file|string block input.
+str: "<div>..</div><div>..</div><p>..</p>"
+in : div out: duplicate
+in : p out: uniqiue
+
+7. Find sum of weight units in "mg" unit. 
+in : "1kg 2gm 200mg + 5kg 5gm 20mg" out: xyz mg
+
+8. Split a string into parts of 3 chars in each part.
+in : "abcd3ef785g" out: [abc, d3e, f78, 5g]
+
+9. Problem name: separate thousand problem.
+in : 1000 out: 1,000
+in : -10000 out: -10,000
+in : -1340000.5000 out: -1,340,000.500
+
+
+Status:
+- Unfinished: 9
+- Problems: 6
 
 */
 }
