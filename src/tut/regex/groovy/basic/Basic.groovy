4cc0405b54642c327405e785fd0fe0fbe1d5b446
omar <omar@omar-NV55C>
2014-07-08 12:27:50 +0600

    Revert "some fixing"
    This reverts commit 4cc1c8dc136f9254d54b5fe8e4b2f58b74334319.

--- src/tut/regex/groovy/basic/Basic.groovy
+++ src/tut/regex/groovy/basic/Basic.groovy
@@ -18,6 +18,22 @@ testStr = """
 		ok ..
 """
 
+/*replaceAllNameWord();
+ captureNames();
+ mobileNoParsing();
+ emailAddressParsing();*/
+//duplicateWordsCount();
+//validateEmailAddress("xy@hgkjjk@dfdfdfd.com");
+//addressMatcher("0.0.0.0");
+//dateValidator("2278/12/20145");
+//timeValidator("00:59:59")
+//fileReader("./src/tut/regex/groovy/basic/temp.txt");
+//sumOfWeightUnits("10kg 5gm+10kg 5gm");
+//tagParser();
+//duplicateTagFinder("span");
+//splitByLookAround("123xyz89df7jk");
+separateThousandProblem("10000000");
+
 /**
  * Use of ?i flag and replaceAll method
  * ReplaceAll "name" word
@@ -41,6 +57,94 @@ def emailAddressParsing(){
 	}
 
 }
+def duplicateWordsCount(){
+
+	def duplicateWords = testStr =~ /\w+/
+	while(duplicateWords.find()){
+		println duplicateWords.group(0);
+	}
+	def map =[:]
+	(0..<duplicateWords.count).each{
+		def val = duplicateWords[it].trim()
+		if(!map.containsKey(val))
+			map[val]=1
+		else
+			map[val]+=1
+
+		/*(0..<duplicateWords.count).each{
+		 println duplicateWords[it]
+		 }*/
+	}
+	map.each {key,val->
+		println key+":"+val;
+
+	}
+}
+
+
+def validateEmailAddress(String str){
+	//def isValidate = str =~ /\w+(?:\.\w+)*@\w+(?:\.\w+)*/
+	//println isValidate[0]
+	def isValidate = str =~ /^\w+(?:\.\w+)*@\w+(?:\.\w+)*$/
+	if(isValidate)
+		println "Valid";
+	else
+		println "Invalid"
+
+}
+
+def addressMatcher(String str){
+
+	def isValidate = str =~ /^(([03-9][0-9]{0,1}|1[0-9]{0,2}|2[0-5]{0,2})(\.|$)){4}/
+	if(isValidate)
+		println "Valid";
+	else
+		println "Invalid"
+}
+def dateValidator(String str){
+	def isValidate = str =~ /^([0-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/(1[0-9]{3}|(200[0-9]|201[0-4]))$/
+	if(isValidate)
+		println "Valid"
+	else
+		println "Invalid"
+}
+
+def timeValidator(String str){
+	def isValidate = str =~ /^(0[0-9]|1[0-2]):[0-5][0-9]:[0-5][0-9]$/
+	if(isValidate)
+		println "Valid"
+	else
+		println "Invalid"
+
+}
+def sumOfWeightUnits(String str){
+	//def number = str =~ /(\d+)\s*([a-z]+)/
+	def splitLine = str =~ /[^\+]+/
+	long res = 0
+
+	while(splitLine.find()){
+		def number = splitLine.group(0)
+		number = splitLine =~ /(\d+)\s*([a-z]+)/
+
+		(0..<number.count).each{
+			if(number[it][2] == 'kg'){
+				res += Integer.parseInt(number[it][1])*1000*1000
+				println "$res"
+			}
+			else if(number[it][2] == 'gm'){
+				res += Integer.parseInt(number[it][1])*1000
+				println Integer.parseInt(number[it][1])*1000
+			}
+			else(number[it][2] == 'mg')
+			res += Integer.parseInt(number[it][1])
+
+
+		}
+	}
+
+	println "$res"
+
+}
 
 /**
  *Use of :
@@ -61,3 +165,67 @@ def captureNames(){
 	//		println names[it][1]
 	//	}
 }
+
+def fileReader(String fileName){
+	File file = new File(fileName);
+
+	println file.getText()
+}
+
+
+def tagParser(){
+
+	def str ="""<div name ="sdsd">
+		<span name ="xyz"> 
+		alit 
+		</span>
+		</div>
+		<a name="xyz"> 
+		alit 
+		</a>"""
+	def tagLine = str =~ /(?s)<(\w+).*?<\/\1>/
+	def tagname = str =~ /(?s)<(\w+).*?/
+	//str = str =~ /(\w*)/
+	while(tagLine.find()){
+		println tagLine.group(0);
+	}
+}
+
+def duplicateTagFinder(String tag){
+
+	def str ="""<div name ="sdsd">
+			   <span name ="xyz"> 
+			alit 
+			</span>
+			</div>
+			<a name="xyz"> 
+			alit 
+			</a>"""
+	patt = "(?s)<(" + tag + ")\b.*?<\1";
+	def match = str =~ patt
+	if(match)
+		println "Duplicate"
+	else
+		println "Unique"
+
+}
+
+def splitByLookAround(def str){
+	
+	println str.split(/(?<=\G.{3})(?=.)/)
+	
+	def splitArr = str.split(/(?<=\G.{3})(?=.)/)
+	splitArr.each{ val->
+		println "val::"+val
+	}
+	
+}
+
+def separateThousandProblem(def str){
+	
+   println str.replaceAll(/(?<=\G\d{1,3})(?=(?:\d{3})+(?!\d))/,",")
+	
+}
+
+
+
